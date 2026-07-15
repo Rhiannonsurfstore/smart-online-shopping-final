3087ddab9e597377313a22fb82606e267ac828a9
Abayisenga Ziripa <ziripaa@gmail.com>
2026-07-15 16:10:13 +0200

    Add TiDB SSL database connection

--- config/database.php
+++ config/database.php
@@ -6,20 +6,34 @@ $password = getenv("DB_PASSWORD");
 $database = getenv("DB_NAME");
 $port = getenv("DB_PORT");
 
+$conn = mysqli_init();
 
-$conn = mysqli_connect(
+/*
+ TiDB Cloud requires SSL/TLS connection
+*/
+mysqli_ssl_set(
+    $conn,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL
+);
+
+mysqli_real_connect(
+    $conn,
     $host,
     $username,
     $password,
     $database,
-    $port
+    $port,
+    NULL,
+    MYSQLI_CLIENT_SSL
 );
 
 
 if (!$conn) {
-
     die("Database connection failed: " . mysqli_connect_error());
-
 }
 
 ?>
