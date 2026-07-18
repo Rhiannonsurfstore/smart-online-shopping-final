0152d52302a9aedb94cbc732e44b27800c35f0dc
Abayisenga Ziripa <ziripaa@gmail.com>
2026-07-18 19:55:33 +0200

    Fix session header warning in navbar

--- includes/Navbar.php
+++ includes/Navbar.php
@@ -1,20 +1,12 @@
-<?php
-if (session_status() !== PHP_SESSION_ACTIVE) {
-    session_start();
-}
-?>
-
 <nav class="navbar navbar-expand-lg navbar-dark sticky-top">
 
 <div class="container">
 
 <a class="navbar-brand" href="index.php">
-
-<i class="fas fa-shopping-cart"></i>
-
-Smart Online Shopping
-
+    <i class="fas fa-shopping-cart"></i>
+    Smart Online Shopping
 </a>
+
 
 <button class="navbar-toggler"
 type="button"
@@ -25,41 +17,52 @@ data-bs-target="#navbarMenu">
 
 </button>
 
+
 <div class="collapse navbar-collapse" id="navbarMenu">
 
 <ul class="navbar-nav ms-auto align-items-center">
 
+
 <li class="nav-item">
 <a class="nav-link" href="index.php">
-<i class="fas fa-home me-1"></i> Home
+    <i class="fas fa-home me-1"></i> Home
 </a>
 </li>
+
 
 <li class="nav-item">
 <a class="nav-link" href="products.php">
-<i class="fas fa-box-open me-1"></i> Products
+    <i class="fas fa-box-open me-1"></i> Products
 </a>
 </li>
+
 
 <li class="nav-item">
 <a class="nav-link" href="cart.php">
-<i class="fas fa-shopping-cart me-1"></i> Cart
+    <i class="fas fa-shopping-cart me-1"></i> Cart
 </a>
 </li>
 
-<?php if(isset($_SESSION['user_id'])){ ?>
+
+
+<?php if(isset($_SESSION['user_id'])) { ?>
+
 
 <li class="nav-item">
 <span class="nav-link">
 
 <i class="fas fa-user-circle me-1"></i>
 
-<?php echo $_SESSION['full_name']; ?>
+<?php 
+echo htmlspecialchars($_SESSION['full_name']); 
+?>
 
 </span>
 </li>
 
+
 <li class="nav-item">
+
 <a class="btn btn-danger ms-3" href="logout.php">
 
 <i class="fas fa-sign-out-alt"></i>
@@ -67,11 +70,16 @@ data-bs-target="#navbarMenu">
 Logout
 
 </a>
+
 </li>
+
+
 
 <?php } else { ?>
 
+
 <li class="nav-item">
+
 <a class="btn btn-info ms-3" href="register.php">
 
 <i class="fas fa-user-plus"></i>
@@ -79,9 +87,13 @@ Logout
 Register
 
 </a>
+
 </li>
 
+
+
 <li class="nav-item">
+
 <a class="btn btn-success ms-2" href="login.php">
 
 <i class="fas fa-sign-in-alt"></i>
@@ -89,9 +101,12 @@ Register
 Login
 
 </a>
+
 </li>
 
+
 <?php } ?>
+
 
 </ul>
 
